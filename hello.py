5047127ceb167c1abcfdf78f75a4fcac1a7e5d94
feliseyra <nilsonii.nt@gmail.com>
2017-06-16 17:32:16 +0200

    NEW: added settings for gunicorn

---
+++ hello.py
@@ -0,0 +1,14 @@
+bind = '0.0.0.0:8080'
+
+
+def get_body(environ):
+	body = environ["QUERY_STRING"].replace('&', '\n')
+	return body.encode('utf-8')
+
+
+def wsgi_application(environ, start_response):
+	status = "200 OK"
+	headers = [("Content-Type", 'text/plain')]
+	body = get_body(environ)
+	start_response(status, headers)
+	return body
